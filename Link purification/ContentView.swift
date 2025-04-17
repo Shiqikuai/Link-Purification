6dde3689d718afee12881145ac0b5fc8dff0cd35
Shiqikuai <shiqikuai@vip.qq.com>
2025-04-17 22:09:29 +0800

    Initial Commit

---
+++ Link purification/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Link purification
+//
+//  Created by shiqikuai on 2025/4/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Link purification/Link_purificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Link_purificationApp.swift
+//  Link purification
+//
+//  Created by shiqikuai on 2025/4/17.
+//
+
+import SwiftUI
+
+@main
+struct Link_purificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
